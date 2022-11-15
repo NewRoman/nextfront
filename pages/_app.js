f8461b42416221a71e47752f9e279199c583e74c
roman.horbunov <roman.horbunov@rolique.io>
2022-11-15 11:42:29 +0200

    Fix config

--- pages/_app.js
+++ pages/_app.js
@@ -9,15 +9,65 @@ import "@/styles/index.css"
 
 const MyApp = ({ Component, pageProps }) => {
   // Extract the data we need
+  // const { global } = pageProps
+  // if (global == null) {
+  //   return <ErrorPage statusCode={404} />
+  // }
 
+  // const { metadata, favicon, metaTitleSuffix } = global.attributes
 
-
-
+  console.log('component', Component, 'pageProps', pageProps)
   return (
     <>
-      <h1>Hello i am next page</h1>
+      {/* Favicon */}
+      {/* <Head>
+        <link
+          rel="shortcut icon"
+          href={getStrapiMedia(favicon.data.attributes.url)}
+        />
+      </Head> */}
+      {/* Global site metadata */}
+      {/* <DefaultSeo
+        titleTemplate={`%s | ${metaTitleSuffix}`}
+        title="Page"
+        description={metadata.metaDescription}
+        openGraph={{
+          images: Object.values(
+            metadata.shareImage.data.attributes.formats
+          ).map((image) => {
+            return {
+              url: getStrapiMedia(image.url),
+              width: image.width,
+              height: image.height,
+            }
+          }),
+        }}
+        twitter={{
+          cardType: metadata.twitterCardType,
+          handle: metadata.twitterUsername,
+        }}
+      /> */}
+      {/* Display the content */}
+      {/* <Component {...pageProps} /> */}
     </>
   )
+}
+
+// getInitialProps disables automatic static optimization for pages that don't
+// have getStaticProps. So [[...slug]] pages still get SSG.
+// Hopefully we can replace this with getStaticProps once this issue is fixed:
+// https://github.com/vercel/next.js/discussions/10949
+MyApp.getInitialProps = async (appContext) => {
+  // Calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext)
+  // const globalLocale = await getGlobalData(appContext.router.locale)
+
+  return {
+    ...appProps,
+    // pageProps: {
+    //   global: globalLocale,
+    // },
+  }
 }
 
 export default MyApp
