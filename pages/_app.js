bd9c1f65bdda32329312e6ac9eef0d592d2a9254
roman.horbunov <roman.horbunov@rolique.io>
2022-11-15 11:55:50 +0200

    Fix config

--- pages/_app.js
+++ pages/_app.js
@@ -60,7 +60,7 @@ const MyApp = ({ Component, pageProps }) => {
 MyApp.getInitialProps = async (appContext) => {
   // Calls page's `getInitialProps` and fills `appProps.pageProps`
   const appProps = await App.getInitialProps(appContext)
-  // const globalLocale = await getGlobalData(appContext.router.locale)
+  const globalLocale = await getGlobalData(appContext.router.locale)
 
   return {
     ...appProps,
